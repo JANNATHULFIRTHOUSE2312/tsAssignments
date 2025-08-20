1438a309133384de50ca186286375bd252621db7
JANNATHULFIRTHOUSE2312 <jannathul.a@bounteous.com>
2025-08-20 15:14:13 +0530

    Update greet.ts

--- greet.ts
+++ greet.ts
@@ -1,1 +1,11 @@
+type User = {
+    firstName: string;
+    lastName: string;
+};
 
+function greet(suser: User): string {
+    return `Hello, ${user.firstName} ${user.lastName}!`;
+}
+
+const customer: User = { firstName: "Jannathul", lastName: "firthouse" };
+console.log(greet(customer));
